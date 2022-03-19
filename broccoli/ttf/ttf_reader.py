ef3bf920d997737b54e22fa8e0f56197d7217003
markountalan <mzguntalan@gmail.com>
2022-03-19 20:09:36 +0800

    Refactored Iterable to Iterator, Some improvements
    - cleaned up recursion
    - added type hints

--- broccoli/ttf/ttf_reader.py
+++ broccoli/ttf/ttf_reader.py
@@ -1,8 +1,7 @@
-from itertools import tee, chain, repeat
-
+from itertools import chain
 from fontTools.ttLib import TTFont
 from xml.etree import ElementTree
-from typing import Iterable, Optional, List
+from typing import Iterator
 from pathlib import Path
 from os.path import isabs
 from broccoli.glyph import Glyph
@@ -17,7 +16,7 @@ from functools import reduce
 class TTFReader:
     def __init__(
         self,
-        glyph_names_to_read_in_font_files: Iterable,
+        glyph_names_to_read_in_font_files: Iterator[str],
         num_points_for_approximation: int,
     ):
         self._glyph_names = glyph_names_to_read_in_font_files
@@ -55,23 +54,12 @@ class TTFReader:
             font_name, self._get_glyphs_from_font_xml(font_xml)
         )
 
-    def _get_font_from_glyphs(self, font_name: str, glyphs: Iterable[Glyph]) -> Font:
+    def _get_font_from_glyphs(self, font_name: str, glyphs: Iterator[Glyph]) -> Font:
         return Font(font_name, glyphs)
 
-    def _get_glyphs_from_font_xml(self, font_xml: ElementTree) -> Iterable[Glyph]:
+    def _get_glyphs_from_font_xml(self, font_xml: ElementTree) -> Iterator[Glyph]:
         root_of_xml = font_xml.getroot()
         ttglyphs = root_of_xml.findall("glyf")[0].findall("TTGlyph")
-
-        # get_glyph_from_ttglyph = lambda ttglyph: Glyph(
-        #     glyph_name=get_name_of_ttglyph(ttglyph),
-        #     vector_graphic_of_the_glyph=sum(
-        #         map(
-        #             lambda contour: self._get_vector_graphic_from_contour(contour),
-        #             map(get_contour_from_ttcontour, get_ttcontours_of_ttglyph(ttglyph)),
-        #         )
-        #     ),
-        # )
-
         desired_ttglyphs = filter(
             lambda g: self._get_name_of_ttglyph(g) in self._glyph_names, ttglyphs
         )
@@ -83,23 +71,16 @@ class TTFReader:
     def _get_glyph_from_ttglyph(self, ttglyph):
         get_ttcontours_of_ttglyph = lambda g: g.findall("contour")
         get_ttpoints_in_ttcontour = lambda ttcontour: ttcontour.findall("pt")
-        get_point_from_ttpoint_bounded_by_min_and_max = (
-            lambda ttpoint, min_x, max_x, min_y, max_y: Point(
-                x=max(min(float(ttpoint.attrib["x"]), max_x), min_x),
-                y=max(min(float(ttpoint.attrib["y"]), max_y), min_y),
-                h=float(ttpoint.attrib["on"]),
-            )
+        get_point_from_ttpoint_bounded_by_min_and_max = lambda ttpoint: Point(
+            x=float(ttpoint.attrib["x"]),
+            y=float(ttpoint.attrib["y"]),
+            h=float(ttpoint.attrib["on"]),
         )
-        # get_contour_from_points = lambda points: Contour(points)
 
         get_contour_from_ttcontour = lambda ttcontour: Contour(
             map(
                 get_point_from_ttpoint_bounded_by_min_and_max,
                 get_ttpoints_in_ttcontour(ttcontour),
-                repeat(-10_000),
-                repeat(10_000),
-                repeat(-10_000),
-                repeat(10_000),
             )
         )
 
@@ -113,25 +94,19 @@ class TTFReader:
         return Glyph(glyph_name=glyph_name, vector_graphic_of_the_glyph=vg)
 
     def _get_vector_graphic_from_contour(self, contour: Contour) -> VectorGraphic:
-        vg = self._get_vector_graphic_sequence_from_point_sequence(
+        vg_sequence = self._get_vector_graphic_sequence_from_point_sequence(
             contour.point_sequence
         )
-        vg, vg2 = tee(vg)
-        vg2 = list(vg2)
-        s = vg2[0]
-        for v in vg2[1:]:
-            s += v
-
-        return reduce(lambda vg_0, vg_1: vg_0 + vg_1, vg)
+        vg: VectorGraphic = reduce(lambda vg_0, vg_1: vg_0 + vg_1, vg_sequence)
+        vg.num_points_for_approximation = self._num_points_for_approximation
+        return vg
 
     def _get_vector_graphic_sequence_from_point_sequence(
         self,
-        point_sequence: Iterable[Point],
-        vector_graphic_sequence: Iterable[VectorGraphic] = (),
-        points_read: Iterable[Point] = (),
-    ) -> Iterable:
-        point_sequence = iter(point_sequence)
-        points_read = iter(points_read)
+        point_sequence: Iterator[Point],
+        vector_graphic_sequence: Iterator[VectorGraphic] = iter([]),
+        points_read: Iterator[Point] = iter([]),
+    ) -> Iterator[VectorGraphic]:
         incoming_point = next(point_sequence, None)
         if incoming_point is None:
             return vector_graphic_sequence
@@ -179,34 +154,3 @@ class TTFReader:
             chain(vector_graphic_sequence, iter([vector_graphic])),
             points_read,
         )
-        #
-        # if last_point_used_for_building_last_vector_graphic is None:
-        #     second_incoming_point = next(point_sequence, None)
-        #     last_incoming_point = second_incoming_point
-        #     if second_incoming_point is None:
-        #         raise ValueError(
-        #             "Point sequence contains a point that belongs to no group."
-        #         )
-        #
-        #     if second_incoming_point.is_off():
-        #         third_incoming_point = next(point_sequence)
-        #         last_incoming_point = third_incoming_point
-        #
-        #         vector_graphic = QuadraticCurve(
-        #             start_point=incoming_point.get_as_xy_tuple(),
-        #             end_point=third_incoming_point.get_as_xy_tuple(),
-        #             control_point=second_incoming_point.get_as_xy_tuple(),
-        #             num_points_for_approximation=self._num_points_for_approximation,
-        #         )
-        #     else:
-        #         vector_graphic = Line(
-        #             start_point=incoming_point.get_as_xy_tuple(),
-        #             end_point=second_incoming_point.get_as_xy_tuple(),
-        #             num_points_for_approximation=self._num_points_for_approximation,
-        #         )
-        #
-        #     return self._get_vector_graphic_sequence_from_point_sequence(
-        #         point_sequence,
-        #         chain(vector_graphic_sequence, iter([vector_graphic])),
-        #         last_point_used_for_building_last_vector_graphic=last_incoming_point,
-        #     )
