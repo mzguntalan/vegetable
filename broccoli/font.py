46dcad3f83b9583f1df4fb6a4741a3395a66ce58
markountalan <mzguntalan@gmail.com>
2022-03-19 23:02:59 +0800

    Add parameter to Font for how many points its glyphs are at default

--- broccoli/font.py
+++ broccoli/font.py
@@ -3,9 +3,17 @@ from broccoli.glyph import Glyph
 
 
 class Font:
-    def __init__(self, font_name: str, glyphs: Iterator[Glyph]):
+    def __init__(
+        self,
+        font_name: str,
+        glyphs: Iterator[Glyph],
+        num_points_in_point_sequence: int = 128,
+    ):
         self._font_name = font_name
-        self._glyphs = {glyph.name: glyph for glyph in glyphs}
+        self._glyphs = {}
+        for glyph in glyphs:
+            glyph.num_points_for_approximation = num_points_in_point_sequence
+            self._glyphs[glyph.name] = glyph
 
     @property
     def name(self):
